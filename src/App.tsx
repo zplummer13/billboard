6c204e5223715f05a237a8145f9ecce254209f7a
zplummer13 <zplummer13@gmail.com>
2022-06-06 22:40:50 -0400

    basic layout and buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SoundButton from './SoundButton';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="h-screen p-6 bg-gradient-to-t from-blue-900 to-blue-600 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8">
+            <SoundButton></SoundButton>
+            <SoundButton></SoundButton>
+            <SoundButton></SoundButton>
+            <SoundButton></SoundButton>
+            <SoundButton></SoundButton>
+            <SoundButton></SoundButton>
+            <SoundButton></SoundButton>
+            <SoundButton></SoundButton>
+            <SoundButton></SoundButton>
+            <SoundButton></SoundButton>
+        </div>
+        // <div className="App">
+        //     <header className="App-header">
+        //         <img src={logo} className="App-logo" alt="logo" />
+        //         <p>
+        //             Edit <code>src/App.tsx</code> and save to reload.
+        //         </p>
+        //         <a
+        //             className="App-link"
+        //             href="https://reactjs.org"
+        //             target="_blank"
+        //             rel="noopener noreferrer"
+        //         >
+        //             Learn React
+        //         </a>
+        //     </header>
+        // </div>
+    );
 }
 
 export default App;
---
+++ src/SoundButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SoundButton() {
+    return (
+        <div className="bg-slate-50 rounded-md shadow-md text-center p-2 flex items-center justify-center">
+            Button
+        </div>
+    )
+}
+
+export default SoundButton;
