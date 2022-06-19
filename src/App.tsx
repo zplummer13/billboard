6ba57c9d4e749ba42b9c29d4983b7ea5c2c5febc
zplummer13 <zplummer13@gmail.com>
2022-06-19 11:35:11 -0400

    add sounds files and play logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,165 +3,140 @@ import SoundButton from "./SoundButton";
 import Welcome from "./Welcome";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
+
+import dad from './audio/dad.mp3';
+import hardbraking from './audio/hard-braking.mp3';
+import lime from './audio/lime.mp3';
+import peppers from './audio/peppers.mp3';
+import spotify from './audio/spotify.mp3';
+import triplethreat from './audio/triple-threat.mp3';
+import unique from './audio/unique.mp3';
+import wildcat from './audio/wildcat.mp3';
+import winning from './audio/winning.mp3';
+import working from './audio/working.mp3';
+
 function App() {
-  const [welcomeClicked, setWelcomeClicked] = useState(false);
-  const buttonMeta = {
-    buttons: [
-      {
-        alt: "pepper monologue",
-        ico: (
-          <FontAwesomeIcon
-            style={{
-              height: "40%",
-              maxHeight: "40%",
-              color: "#0D486F",
-            }}
-            icon={solid("pepper-hot")}
-          />
-        ),
-        link: "",
-      },
-      {
-        alt: "triple threat",
-        ico: (
-          <FontAwesomeIcon
-            style={{
-              height: "40%",
-              maxHeight: "40%",
-              color: "#0D486F",
-            }}
-            icon={solid("clapperboard")}
-          />
-        ),
-        link: "",
-      },
-      {
-        alt: "winning",
-        ico: (
-          <FontAwesomeIcon
-            style={{
-              height: "40%",
-              maxHeight: "40%",
-              color: "#0D486F",
-            }}
-            icon={solid("trophy")}
-          />
-        ),
-        link: "",
-      },
-      {
-        alt: "cheaters?",
-        ico: (
-          <FontAwesomeIcon
-            style={{
-              height: "40%",
-              maxHeight: "40%",
-              color: "#0D486F",
-            }}
-            icon={solid("glasses")}
-          />
-        ),
-        link: "",
-      },
-      {
-        alt: "wildcat",
-        ico: (
-          <FontAwesomeIcon
-            style={{
-              height: "40%",
-              maxHeight: "40%",
-              color: "#0D486F",
-            }}
-            icon={solid("paw")}
-          />
-        ),
-        link: "",
-      },
-      {
-        alt: "john williams music",
-        ico: (
-          <FontAwesomeIcon
-            style={{
-              height: "40%",
-              maxHeight: "40%",
-              color: "#0D486F",
-            }}
-            icon={solid("hat-cowboy")}
-          />
-        ),
-        link: "",
-      },
-      {
-        alt: "alexa play bills poolside playlist",
-        ico: (
-          <FontAwesomeIcon
-            style={{
-              height: "40%",
-              maxHeight: "40%",
-              color: "#0D486F",
-            }}
-            icon={solid("volume-high")}
-          />
-        ),
-        link: "",
-      },
-      {
-        alt: "slippery nipple?",
-        ico: (
-          <FontAwesomeIcon
-            style={{
-              height: "40%",
-              maxHeight: "40%",
-              color: "#0D486F",
-            }}
-            icon={solid("martini-glass-citrus")}
-          />
-        ),
-        link: "",
-      },
-      {
-        alt: "that's unique",
-        ico: (
-          <FontAwesomeIcon
-            style={{
-              height: "40%",
-              maxHeight: "40%",
-              color: "#0D486F",
-            }}
-            icon={solid("face-meh")}
-          />
-        ),
-        link: "",
-      },
-      {
-        alt: "I Dad it",
-        ico: (
-          <FontAwesomeIcon
-            style={{
-              height: "40%",
-              maxHeight: "40%",
-              color: "#0D486F",
-            }}
-            icon={solid("user-tie")}
-          />
-        ),
-        link: "",
-      },
-    ],
-  };
-  return (
-    <>
-      {welcomeClicked ? (
-        <div className="h-full p-9 bg-gradient-to-t from-sky-900 to-blue-900 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8">
-          {buttonMeta.buttons.map((buttonMeta) => {
-            return <SoundButton buttonInfo={buttonMeta} />;
-          })}
+    const [welcomeClicked, setWelcomeClicked] = useState(false);
+
+    const iconClassName = "text-blue-900 h-1/2"
+
+    const buttonMeta = {
+        buttons: [
+            {
+                alt: "lime",
+                ico: (
+                    <FontAwesomeIcon
+                        className={iconClassName}
+                        icon={solid("martini-glass-citrus")}
+                    />
+                ),
+                link: lime,
+            },
+            {
+                alt: "pepper monologue",
+                ico: (
+                    <FontAwesomeIcon
+                        className={iconClassName}
+                        icon={solid("pepper-hot")}
+                    />
+                ),
+                link: peppers,
+            },
+            {
+                alt: "that's unique",
+                ico: (
+                    <FontAwesomeIcon
+                        className={iconClassName}
+                        icon={solid("face-meh")}
+                    />
+                ),
+                link: unique,
+            },
+            {
+                alt: "winning",
+                ico: (
+                    <FontAwesomeIcon
+                        className={iconClassName}
+                        icon={solid("trophy")}
+                    />
+                ),
+                link: winning,
+            },
+            {
+                alt: "working",
+                ico: (
+                    <FontAwesomeIcon
+                        className={iconClassName}
+                        icon={solid("thumbs-up")}
+                    />
+                ),
+                link: working,
+            },
+            {
+                alt: "wildcat",
+                ico: (
+                    <FontAwesomeIcon
+                        className={iconClassName}
+                        icon={solid("paw")}
+                    />
+                ),
+                link: wildcat,
+            },
+            {
+                alt: "hard braking",
+                ico: (
+                    <FontAwesomeIcon
+                        className={iconClassName}
+                        icon={solid("car-on")}
+                    />
+                ),
+                link: hardbraking,
+            },
+            {
+                alt: "alexa play bills poolside playlist",
+                ico: (
+                    <FontAwesomeIcon
+                        className={iconClassName}
+                        icon={solid("volume-high")}
+                    />
+                ),
+                link: spotify,
+            },
+            {
+                alt: "triple threat",
+                ico: (
+                    <FontAwesomeIcon
+                        className={iconClassName}
+                        icon={solid("clapperboard")}
+                    />
+                ),
+                link: triplethreat,
+            },
+            {
+                alt: "I Dad it",
+                ico: (
+                    <FontAwesomeIcon
+                        className={iconClassName}
+                        icon={solid("user-tie")}
+                    />
+                ),
+                link: dad,
+            },
+        ],
+    };
+    return (
+        <div className="h-full">
+            {welcomeClicked ? (
+                <div className="h-full p-8 bg-gradient-to-t from-sky-900 to-blue-900 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8">
+                    {buttonMeta.buttons.map((buttonMeta) => {
+                        return <SoundButton buttonInfo={buttonMeta} />;
+                    })}
+                </div>
+            ) : (
+                <Welcome setWelcomeClicked={setWelcomeClicked} />
+            )}
         </div>
-      ) : (
-        <Welcome setWelcomeClicked={setWelcomeClicked} />
-      )}
-    </>
-  );
+    );
 }
 
 export default App;
--- src/SoundButton.tsx
+++ src/SoundButton.tsx
@@ -1,25 +1,32 @@
 import React from "react";
+import useSound from "use-sound";
 
 type SoundButtonProps = {
-  buttonInfo: {
-    alt: string;
-    ico: JSX.Element;
-    link: string;
-  };
+    buttonInfo: {
+        alt: string;
+        ico: JSX.Element;
+        link: any;
+    };
 };
+
+
 function SoundButton(props: SoundButtonProps) {
-  return (
-    <div
-      className="bg-sky-100 rounded-lg border-2 border-sky-200 shadow-lg shadow-inner text-center p-2 flex items-center justify-center"
-      aria-label={props.buttonInfo.alt}
-      onClick={(e) => playLink(props.buttonInfo.link)}
-    >
-      {props.buttonInfo.ico}
-    </div>
-  );
+    const [play] = useSound(props.buttonInfo.link, { interrupt: true });
+
+    return (
+        <div
+            className="bg-sky-100 rounded-lg border-2 border-sky-200 shadow-lg shadow-inner text-center flex items-center justify-center"
+            aria-label={props.buttonInfo.alt}
+            onClick={() => play()}
+        >
+            {props.buttonInfo.ico}
+        </div>
+    );
 }
 
 export default SoundButton;
 function playLink(link: string): void {
-  console.log(link);
+
+
+    console.log(link);
 }
